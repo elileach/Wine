1b40bb12b24115b739917e7f7244aa5d56cea43f
elileach <elileach@vt.edu>
2023-01-24 20:48:26 -0500

    f

--- Wine/forms.py
+++ Wine/forms.py
@@ -1,2 +1,5 @@
 from django import forms
 
+
+class TextInputForm(forms.Form):
+    input_text = forms.CharField(max_length=100)
--- Wine/views.py
+++ Wine/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import TextInputForm
+
 
 # Create your views here.
+def my_view(request):
+    if request.method == 'POST':
+        form = TextInputForm(request.POST)
+        if form.is_valid():
+            input_text = form.cleaned_data['input_text']
+            # do something with input_text
+            return HttpResponse("Input text is : " + input_text)
+    else:
+        form = TextInputForm()
+    return render(request, 'template.html', {'form': form})
